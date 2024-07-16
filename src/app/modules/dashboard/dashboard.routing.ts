acfe7a8d30b494c8b30a2879f9cc9f5897533bbb
KarMiguel <carlosmiguel.dsa12@gmail.com>
2024-07-16 00:18:56 -0300

    feat: :sparkles: creating routings

---
+++ src/app/modules/dashboard/dashboard.routing.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
+
+export const DASHBOARD_ROUTES: Routes = [
+  {
+    path: '',
+    component: DashboardHomeComponent,
+  },
+];
